59f49da1fe9aa6ce5071a6113919debcde403cc4
ok2qw66 <ok2qw66@naver.com>
2022-01-31 00:30:15 +0900

    구현1 문자열압축;

--- 이것이코딩테스트다/구현/문자열압축.py
+++ 이것이코딩테스트다/구현/문자열압축.py
@@ -1,29 +1,31 @@
 def solution(s):
     answer = len(s)
     
-    for base_count in range(int(len(s)/2) + 1, 0, -1):
+    for base_count in range(int(len(s)/2), 0, -1):
         temp_answer = 0
         idx = 0
         while idx < len(s) :
-            count_idx = [0,0]
-            word = s[:base_count]
-            
+            count_idx = [0,idx]
+            word = s[idx:idx+base_count]
+            # print(word)
             while word == s[count_idx[1] : count_idx[1]+base_count]:
                 count_idx[1] += base_count
                 count_idx[0] += 1
-                print(count_idx)
             
+            # print(count_idx)
             if count_idx[0] > 1:
                 temp_answer += len(str(count_idx[0])) + len(word)
+            elif count_idx[0] == 1:
+                temp_answer += + len(word)
             else : 
                 temp_answer += len(s[idx:])
                 break
             idx = count_idx[1]
-            print("idx ", idx)
-        print(base_count, temp_answer)
-        if temp_answer < answer : return temp_answer
+            # print("idx ", idx, " ",temp_answer)
+        # print(base_count, temp_answer)
+        answer = min(answer, temp_answer)
            
     return answer
 
 
-print(solution("aabbaccc"))
+print(solution("abcabcabcdabcfabcabcabczabcabcdddabc"))
